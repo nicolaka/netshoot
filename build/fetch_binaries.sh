46b83d7e9fabaf555c11ed6e45af6a33c76dc485
Dennis Gaida <2392217+DennisGaida@users.noreply.github.com>
2022-02-16 10:51:38 +0100

    ARM is supported in termshark & ctop
    tested building on Raspberrypi4 with arch: armv7l. calicotl isn't available for armv7l so commenting out

--- build/fetch_binaries.sh
+++ build/fetch_binaries.sh
@@ -19,36 +19,37 @@ case $ARCH in
 esac
 
 get_ctop() {
+  TERM_ARCH=$ARCH
+  if [ "$ARCH" == "armv7l" ]; then
+    TERM_ARCH=arm
+  fi
   VERSION=$(get_latest_release bcicen/ctop | sed -e 's/^v//')
-  LINK="https://github.com/bcicen/ctop/releases/download/${VERSION}/ctop-${VERSION}-linux-${ARCH}"
+  LINK="https://github.com/bcicen/ctop/releases/download/${VERSION}/ctop-${VERSION}-linux-${TERM_ARCH}"
   wget "$LINK" -O /tmp/ctop && chmod +x /tmp/ctop
 }
 
 get_calicoctl() {
-  VERSION=$(get_latest_release projectcalico/calicoctl)
-  LINK="https://github.com/projectcalico/calicoctl/releases/download/${VERSION}/calicoctl-linux-${ARCH}"
-  wget "$LINK" -O /tmp/calicoctl && chmod +x /tmp/calicoctl
+  if [ "$ARCH" != "armv7l" ]; then
+    VERSION=$(get_latest_release projectcalico/calicoctl)
+    LINK="https://github.com/projectcalico/calicoctl/releases/download/${VERSION}/calicoctl-linux-${ARCH}"
+    wget "$LINK" -O /tmp/calicoctl && chmod +x /tmp/calicoctl
+  fi
 }
 
 get_termshark() {
-  case "$ARCH" in
-    "arm"*)
-      echo "echo termshark does not yet support arm" > /tmp/termshark && chmod +x /tmp/termshark
-      ;;
-    *)
-      VERSION=$(get_latest_release gcla/termshark | sed -e 's/^v//')
-      if [ "$ARCH" == "amd64" ]; then
-        TERM_ARCH=x64
-      else
-        TERM_ARCH="$ARCH"
-      fi
-      LINK="https://github.com/gcla/termshark/releases/download/v${VERSION}/termshark_${VERSION}_linux_${TERM_ARCH}.tar.gz"
-      wget "$LINK" -O /tmp/termshark.tar.gz && \
-      tar -zxvf /tmp/termshark.tar.gz && \
-      mv "termshark_${VERSION}_linux_${TERM_ARCH}/termshark" /tmp/termshark && \
-      chmod +x /tmp/termshark
-      ;;
-  esac
+  VERSION=$(get_latest_release gcla/termshark | sed -e 's/^v//')
+  if [ "$ARCH" == "amd64" ]; then
+    TERM_ARCH=x64
+  elif [ "$ARCH" == "armv7l" ]; then
+    TERM_ARCH=armv6
+  else
+    TERM_ARCH="$ARCH"
+  fi
+  LINK="https://github.com/gcla/termshark/releases/download/v${VERSION}/termshark_${VERSION}_linux_${TERM_ARCH}.tar.gz"
+  wget "$LINK" -O /tmp/termshark.tar.gz && \
+  tar -zxvf /tmp/termshark.tar.gz && \
+  mv "termshark_${VERSION}_linux_${TERM_ARCH}/termshark" /tmp/termshark && \
+  chmod +x /tmp/termshark
 }
 
 get_ctop
